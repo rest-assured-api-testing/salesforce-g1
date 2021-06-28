165da3923bf2ac958efd83610421b58525eaa380
Raymundo Guaraguara <raymundogs@gmail.com>
2021-06-28 18:38:51 -0400

    delete unnecesary method

--- src/main/java/utils/DataType.java
+++ src/main/java/utils/DataType.java
@@ -10,25 +10,11 @@
 package utils;
 
 import utils.strategy.ObjectsNames;
-import java.lang.reflect.Field;
 
 /**
  * This class provides methods to compare and convert data types
  */
 public class DataType {
-    /**
-     * Checks if the data types matches between objects
-     * @param firstObject the first object to check
-     * @param secondObject the second object to match
-     * @return a boolean with the result
-     */
-    public static boolean validateDataType(final Field firstObject, final Object secondObject) {
-        if (firstObject.getType().equals(secondObject.getClass())) {
-            return true;
-        }
-        return false;
-    }
-
     /**
      * Converts an object type to another
      * @param value a String with the value to convert
